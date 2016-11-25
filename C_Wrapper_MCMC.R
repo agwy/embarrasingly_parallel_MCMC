ae56d72eeaf27a44674d9695163405f6192ed948
Dominic Richards <richards@thornbird.stats.ox.ac.uk>
2016-11-25 18:05:53 +0000

    C implementations and wrapper

---
+++ C_Wrapper_MCMC.R
@@ -0,0 +1,18 @@
+#MHCMCMC wrapper
+MCMC_MH <- function(Iterations, Data_Matrix, Obs,Inital_beta,proposal_sd) {
+  ans <- .C("MCMC", 
+            as.integer(Iterations),
+            as.integer(length(Obs)),
+            as.integer(length(Inital_beta)),
+            as.double(Data_Matrix),
+            as.integer(Obs),
+            as.double(proposal_sd),
+            as.double(Inital_beta),
+            acceptance_rate = as.double(1:Iterations),
+            Result = as.double(1:(length(Inital_beta)*(Iterations+1)))
+            )
+  print("HEllo world")
+  return(list(Result = matrix(ans$Result,ncol=length(Inital_beta), byrow=TRUE),
+              Acceptance_rate = ans$acceptance_rate))
+}
+
