47847cbb0a0180d28ef806bfaa3bb0057dcbf513
Arne Gouwy <gouwy@treecreeper.stats.ox.ac.uk>
2016-11-26 17:57:36 +0000

    Probit example updates for C call.

--- Probit_example.R
+++ Probit_example.R
@@ -2,37 +2,25 @@
 library(Rcpp)
 library(mvtnorm)
 library(parallel)
+library(profr)
 
-
-source("probit_funcs.R")
-source("MH_MCMC_chain.R")
+library(embarrassinglyParallelProbitMCMC, lib.loc="Packages")
+# detach?
+# source...
 
 probit_dimension <- 50
 obs_count <- 1e4
 
 simulated_probit_data <- sim_probit(obs_count,probit_dimension)
 
-#Test density functions and use standard GLM functions
-
-glm_test <- glm(simulated_probit_data$obs~simulated_probit_data$design_mat + 0,family = binomial(link="probit"))
-
-
-probit_den(observations = simulated_probit_data$obs, 
-           beta = glm_test$coefficients,
-           design_mat = simulated_probit_data$design_mat)
-
-augmented_density(observations = simulated_probit_data$obs, 
-                  beta = glm_test$coefficients,
-                  design_mat = simulated_probit_data$design_mat,
-                  to_log=T)
-
-
-#MCMC approximation
+## MCMC approximation
 
 total_iterations <- 10000
 
-
+# R Implementation of a MCMC chain:
+source("probit_funcs.R")
 source("MH_MCMC_chain.R")
+Rprof("timecheck.out")
 test_MCMC <- MH_MCMC_chain(
   Iterations = total_iterations,
   target_density = augmented_density,
@@ -41,13 +29,21 @@ test_MCMC <- MH_MCMC_chain(
   observations=simulated_probit_data$obs,
   design_mat=simulated_probit_data$design_mat,
   to_log = T)
+summaryRprof("timecheck.out")
+
+# C implementation of a MCMC chain:
+Rprof("timecheck_c.out")
+test_MCMC_c <- MCMC_MH(1, 
+                       total_iterations, 
+                       simulated_probit_data$design_mat, 
+                       simulated_probit_data$obs, 
+                       rep(0, times=probit_dimension), 
+                       0.01)
+summaryRprof("timecheck_c.out")
+print(test_MCMC_c$Acceptance_rate)
 
 
-
-
-
-
-#How do our MCMC approximations compare?
+# MCMC results for the true values, R implementation:
 plot(rowMeans(test_MCMC)-simulated_probit_data$beta)
 
 augmented_density(observations = simulated_probit_data$obs, 
@@ -55,10 +51,7 @@ augmented_density(observations = simulated_probit_data$obs,
                   design_mat = simulated_probit_data$design_mat,
                   to_log=T)
 
-
-
-
-#Parallel implementation
+## Parallel implementation
 
 Chain_count <- 8 #Number of subsets
 
@@ -103,9 +96,22 @@ colMeans(test_nonparametric)
 plot(test_nonparametric[,10])
 abline(h= simulated_probit_data$beta[10])
 
+######################################################
+## TEST CODE 
+
+#Test density functions and use standard GLM functions
+
+glm_test <- glm(simulated_probit_data$obs~simulated_probit_data$design_mat + 0,family = binomial(link="probit"))
 
 
+probit_den(observations = simulated_probit_data$obs, 
+           beta = glm_test$coefficients,
+           design_mat = simulated_probit_data$design_mat)
 
+augmented_density(observations = simulated_probit_data$obs, 
+                  beta = glm_test$coefficients,
+                  design_mat = simulated_probit_data$design_mat,
+                  to_log=T)
 
 
 
