7bfac41cf99bac9958f0c2345833c59ff1286cb1
Dominic Richards <richards@thornbird.stats.ox.ac.uk>
2016-11-26 13:05:22 +0000

    Package Folder

---
+++ OxWaSPModule4/R/MH_MCMC_chain.R
@@ -0,0 +1,36 @@
+MH_MCMC_chain <- function(Iterations,target_density, proposal_sd,inital_value,...){
+  
+   accepted <- rep(0,Iterations)
+   d <- dim(inital_value)[1]
+   
+   output <- matrix(rep(NA,Iterations*d),nrow=d)
+   
+  output[,1] <- inital_value
+  proposed_value <- NA
+  for(j in 2:Iterations){
+    proposed_value <- output[,j-1] + rnorm(d,0,proposal_sd)
+    
+    if(
+      exp(sum(target_density(beta=proposed_value,...)) - sum(target_density(beta=output[,j-1],...)))
+      > runif(1)){
+        output[,j] <- proposed_value
+        accepted[j] <- 1
+    }else{
+      output[,j] <- output[,j-1]
+    }
+    if(k %% floor(Iterations*0.1) == 0 ) print("Iteration: ", k/Iterations)
+  }
+  print(mean(accepted))
+  return(output)
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ OxWaSPModule4/R/NonParametric_Density_Product_Estimates.R
@@ -0,0 +1,58 @@
+#Asymptotically Exact Sampling 
+require(mvtnorm)
+
+#Assume chains in chain_store are of dimension rxn
+
+Weight_cal <- function(t_dot, chain_store,Gaus_Bandwidth){
+  theta_dot <- sapply(1:length(t_dot),function(x) chain_store[[x]][t_dot[x],])
+  theta_bar <- rowMeans(theta_dot)
+  
+  w_dot <- sum(dmvnorm(x = t(theta_dot),mean = theta_bar,diag(Gaus_Bandwidth^2,length(theta_bar)),log = T))
+  return(w_dot)
+}
+
+
+sample_theta <- function(t_dot, chain_store,Gaus_Bandwidth ){
+  theta_dot <- sapply(1:length(t_dot),function(x) chain_store[[x]][t_dot[x],])
+  theta_bar <- rowMeans(theta_dot)
+  return(rmvnorm(n = 1,mean = theta_bar,sigma = diag(Gaus_Bandwidth^2,length(theta_bar))))
+}
+
+#Takes list of Markov Chains whose length is equal, and produces output according to 
+#Algorithm 1 in paper
+nonparametric_implemetation <- function(chain_store,Verbose = TRUE){
+  
+  total_iter <- dim(chain_store[[1]])[1]
+  M <- length(chain_store)
+  d <- dim(chain_store[[1]])[2]
+  
+  t_dot <- sample(1:total_iter,M,replace = T)
+  
+  #For storing Output
+  theta_out  <- matrix(rep(0,d*total_iter),ncol=d)
+  
+  for(k in 1:total_iter){
+    bandwidth_set <- k^(-1/(4 + d))
+    for(j in 1:M){
+      c_dot <- t_dot
+      c_dot[j] <- sample(1:total_iter,1)
+      u <- runif(1)
+      if( u <  exp(Weight_cal(t_dot = c_dot,Gaus_Bandwidth =bandwidth_set,chain_store = chain_store ) - 
+                   Weight_cal(t_dot = t_dot,Gaus_Bandwidth =bandwidth_set,chain_store = chain_store ) 
+                   )
+          ) t_dot <- c_dot
+    }
+    if( (k %% (total_iter* 0.1) == 0)  & Verbose ) print(paste("Iteration: ", k/total_iter))
+    
+    theta_out[k,] <- sample_theta(t_dot,test3,bandwidth_set)
+  }
+  
+  #Faster Implementation -- seeems very serial, what can be vectorised?
+  
+  return(theta_out)
+}
+
+
+
+
+
